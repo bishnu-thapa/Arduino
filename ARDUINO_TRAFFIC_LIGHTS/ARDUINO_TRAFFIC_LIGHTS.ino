678549865f065c106747c7759b87f2d3637e6dce
Paul <zleap@zleap.net>
2015-08-08 18:29:00 +0100

    added red / green man LEDs

--- ARDUINO_TRAFFIC_LIGHTS/ARDUINO_TRAFFIC_LIGHTS.ino
+++ ARDUINO_TRAFFIC_LIGHTS/ARDUINO_TRAFFIC_LIGHTS.ino
@@ -7,9 +7,14 @@
  
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
-int ledred = 4;  // RED LED
-int ledyellow = 7;  // YELLOW LED
-int ledgreen = 8;;  // GREEN LED
+// traffic leds
+int ledred = 4;  // RED LED 5mm
+int ledyellow = 7;  // YELLOW LED 5mm
+int ledgreen = 8;;  // GREEN LED 5mm
+// leds for red / green man
+int ledredM = 10;  // Red LED 3mm
+int ledgreenM = 11;  // Green LED 3mm
+
 
 // the setup routine runs once when you press reset:
 void setup() {                
@@ -17,23 +22,48 @@ void setup() {
   pinMode(ledred, OUTPUT);
   pinMode(ledyellow, OUTPUT);
   pinMode(ledgreen, OUTPUT);  
+  pinMode(ledredM, OUTPUT);
+  pinMode(ledgreenM, OUTPUT);  
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
+  // STOP TRAFFIC AND TURN GREEN MAN ON FOR PEDESTRIANS
+  
   digitalWrite(ledred, HIGH);   // turn the RED LED on (HIGH is the voltage level)
-  delay(5000);               // wait for a second
+  
+  digitalWrite(ledgreenM, HIGH);   // turn the 3mm green man ON
+  
+
+    
+  delay(5000);               // wait for 5 second
+  
+   
+  
   digitalWrite(ledyellow, HIGH);    // turn the YELLOW LED ON by making the voltage HIGH
-  delay(250);               // wait for a second
+  delay(1000);               // wait for a second
+  
+  // NOW TURN GREEN MAN OFF AND RED MAN ON
+  
+  digitalWrite(ledredM, HIGH);   // RED MAN ON
+  digitalWrite(ledgreenM, LOW);    // GREEN MAN OFF
+  
+  
   digitalWrite(ledred, LOW);   // turn the RED OFF on (LOW is the voltage level)
   digitalWrite(ledyellow, LOW);    // turn the YELLOW LED OFF by making the voltage LOW
-  delay(1000);  
   digitalWrite(ledgreen, HIGH);    // turn the GREEN LED ON by making the voltage high
-  delay(5000);               // wait for a second
-  digitalWrite(ledgreen, HIGH);    // turn the GREEN LED OFF by making the voltage HIGH
+  
+  delay(1000);               // wait for a second
+  
+  
+  digitalWrite(ledgreen, HIGH);    // turn the GREEN LED on by making the voltage HIGH
+  
+
+  
   delay(5000);
   digitalWrite(ledgreen, LOW);    // turn the GREEN LED OFF by making the voltage LOW
   digitalWrite(ledyellow, HIGH);    // turn the YELLOW LED ON by making the voltage HIGH
   delay(1000);               // wait for a second
-  
-}
+  digitalWrite(ledyellow, LOW);    // turn the YELLOW LED OFF by making the voltage LOW
+   digitalWrite(ledredM, LOW);   // RED MAN OFF
+} 
