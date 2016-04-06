be1223e4fcc20db030a89d7875e0ea310f1e225f
paul Sutton <zleap@zleap.net>
2016-04-06 16:34:03 +0100

    update 6-4-2016

--- weather_station_serial_output_leds/weather_station_serial_output_leds.ino
+++ weather_station_serial_output_leds/weather_station_serial_output_leds.ino
@@ -1,51 +1,47 @@
 #include "DHT.h"
-#define DHTPIN 8     // pin DHT is connected
+#define DHTPIN 8 // Pin for humidity and temperature sensor
 #define DHTTYPE DHT11  
 
-int templed = 3; // blue ked
-int humidled = 4; // red led
+#define TEMPLED  3 // Blue led for temperature
+#define HUMIDLED 4 // Red led for humidity
 
 DHT dht(DHTPIN, DHTTYPE);
 void setup() {
+  Serial.begin(9600);
   dht.begin();
-  //lcd.begin(16, 2);
-  pinMode(templed, OUTPUT); 
-    pinMode(humidled, OUTPUT); 
+  pinMode(TEMPLED, OUTPUT); 
+  pinMode(HUMIDLED, OUTPUT); 
 }
 void loop() {
-  //digitalWrite(templed, LOW);//turn led off
-  //digitalWrite(humidled, LOW);  // turn led off
-   float h = dht.readHumidity();
+  // Read the temperature and humidity
+  float h = dht.readHumidity();
   float t = dht.readTemperature();
-  // t = t* 9/5 + 32; // display as farenheit
-    t = t; // display as centigrade
-    if (isnan(t) || isnan(h)) {
-    //lcd.setCursor(0, 0);
-    //lcd.print("Failed to read from DHT");
-    //Serial.println("Failed to read from DHT");
-  } else {
-    delay(1000); // add delay to stop flickering screen
-    Serial.begin(9600);
-    Serial.print("Humidity: "); 
-    Serial.print(h);
-    Serial.print(" %\t");
-    Serial.print("Temperature: "); 
-    Serial.print(t);
-    Serial.println(" C");
-    // turn off LEDs
-    
- {  
-if(templed >= 19);  // this number depends temperature
-digitalWrite(templed, HIGH);//turn led ON
-
-}
- {  
-if(humidled >= 47);  // this number depends humidity
-digitalWrite(humidled, HIGH);//turn led ON
-delay(1000);
-}
   
+  // Make sure we actually get some data
+  if (isnan(t) || isnan(h)) {
+    Serial.println("Failed to read from DHT");
+    return;
+  }
+  
+  // Output to serial console
+  Serial.print("Humidity: "); 
+  Serial.println(h);
+  Serial.print("Temperature: "); 
+  Serial.print(t);
+  Serial.println("C");
+  delay(1000);
+
+  if (t >= 20) { // Temperature
+    digitalWrite(TEMPLED, HIGH);
+  } else {
+    digitalWrite(TEMPLED, LOW);
+  }
+
+  if (h >= 47) { // Humidity
+    digitalWrite(HUMIDLED, HIGH);
+  } else {
+    digitalWrite(HUMIDLED, LOW);
   }
 }
 
-//temp and humidity
+//version 2,0 by Samadi
